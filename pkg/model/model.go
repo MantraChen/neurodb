35001208fd97ea63d330a3d172053c0d7f8fc003
MantraChen <194911688+MantraChen@users.noreply.github.com>
2026-02-14 05:31:51 +0800

    Refactor Model interface in model.go by removing redundant comments for improved code clarity while maintaining existing functionality.

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,10 +1,8 @@
 package model
 
-//The model defines how Keys are mapped to Positions.
-
 type Model interface {
-	Traom(keys []int64) error    //Training
-	Predict(Key int64) (pos int) //Predicted position
-	ErrorBound() (min, max int)  //Error range(used for final step search)
-	SizeInBytes() int            //Model size (used for cost evaluation)
+	Traom(keys []int64) error
+	Predict(Key int64) (pos int)
+	ErrorBound() (min, max int)
+	SizeInBytes() int
 }
